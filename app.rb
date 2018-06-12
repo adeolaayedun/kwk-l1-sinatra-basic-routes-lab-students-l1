72b1396c96fd5dbe32300c31c423a3c7ed90dbec
adeolaayedun <deola.ayedun@gmail.com>
2018-06-12 15:51:42 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+      "My name is Ade'ola"
+    end
+
+    get '/hometown' do
+      "My hometown is Brookhaven"
+    end
+
+    get '/favorite-song' do
+      "My favorite song is in love with another man"
+    end
 end
