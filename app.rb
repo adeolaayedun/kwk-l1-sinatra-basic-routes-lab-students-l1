4ee6ce415ff3f3d1977f14250860953ec4c128a3
adeolaayedun <deola.ayedun@gmail.com>
2018-06-12 15:49:05 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,14 +2,14 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
     get '/' do
-      "My name is"
+      "My name is Ade'ola"
     end
 
     get '/' do
-      "My hometown is"
+      "My hometown is Brookhaven"
     end
 
     get '/' do
-      "My favorite song is"
+      "My favorite song is in love with another man"
     end
 end
